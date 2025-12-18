055524c5b1250e050fabfc59f23f5a5959242f99
Jayasuriya-ENGINEER <jayasuriya77721@gmail.com>
2025-12-18 23:12:09 +0530

    sample added

---
+++ sample.py
@@ -0,0 +1,37 @@
+''' print(1,2,3,sep='\n')
+
+#for i in range(5):
+ #   print("*" *i)
+
+print(1+2)
+print((1+2)<4)
+
+print((5%-2)==-1)
+
+print((-2%2)==0)
+
+print(bool(-1))
+print(bool(0))
+print(bool(1))
+
+def f(num):
+    return 'a' if num>0 else 1
+
+print(f(-11))
+
+for i in range(5):
+    print(i) 
+
+t= 'Hello World! '
+
+print(t=="Hello\n World!")
+
+
+a=['a','b','c']
+b=[char+'!' for char in a]
+print(a)
+print(b) '''
+
+dictionary = {'b':1,'z':2,'c':3}
+dictionary
+print((sorted(dictionary.values())))
